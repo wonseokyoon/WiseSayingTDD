c7fff4ce9b542a0b677de503f2a636db6df7dc90
wonseokyoon <01088115030a@gmail.com>
2025-01-02 14:53:47 +0900

    TDD Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner =new Scanner("등록\n현재를 사랑하라\n작자미상\n");
+        String val1= scanner.nextLine();
+        System.out.println("명령\n=================================\n"+ val1);   //aaa
+
+        String val2= scanner.nextLine();
+        System.out.println("명언: "+val2);   //bbb
+
+        String val3= scanner.nextLine();
+        System.out.println("작가: "+val3);   //bbb
     }
 }
