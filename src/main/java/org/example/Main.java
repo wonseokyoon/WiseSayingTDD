db3b8c20659babf67db28d843ce5bee2ee5e45e8
wonseokyoon <01088115030a@gmail.com>
2025-01-02 15:44:54 +0900

    test#3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        test2();
+    }
+    public static void test1() {
 
         Scanner scanner =new Scanner("등록\n현재를 사랑하라\n작자미상\n");
         String val1= scanner.nextLine();
@@ -18,4 +25,19 @@ public class Main {
         TestApp app=new TestApp();
         app.run();
     }
+    public static void test2() {
+        //origin: 모니터 화면
+        PrintStream origin=System.out;
+
+        //비어있는 스트림
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));  // out방향을 스트림으로
+        System.out.println("hello");    //출력 x, 스트림으로 나감
+
+        System.setOut(origin);  // out방향을 콘솔로 다시 설정
+        String str=byteArrayOutputStream.toString();
+        System.out.println(str);    //출력 o
+    }
+
+
 }
--- src/main/java/org/example/TestApp.java
+++ src/main/java/org/example/TestApp.java
@@ -4,6 +4,6 @@ public class TestApp {
 
     public void run() {
         //System.out.println("aaa");
-        return "aaa";
+
     }
 }
