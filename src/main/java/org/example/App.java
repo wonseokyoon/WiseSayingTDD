a9fb07262217c83a224ca85bb7c695ccd536070e
wonseokyoon <01088115030a@gmail.com>
2025-01-05 15:11:07 +0900

    test9 blue

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -45,10 +45,12 @@ public class App {
             else if (cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                System.out.println("2 / 작자미상 / 과거에 집착하지 마라.");
-                System.out.println("1 / 작자미상 / 현재를 사랑하라.");
 
-
+                List<WiseSaying> reversed=new ArrayList<>(wiseSayingList);
+                Collections.reverse(reversed);
+                reversed.forEach(w->{
+                    System.out.printf("%d / %s / %s\n",w.getId(),w.getAuthor(),w.getContent());
+                });
 
 
             }
