43bc670f0e8458e14cb73763c8c0ace3a99f960e
wonseokyoon <01088115030a@gmail.com>
2025-01-05 14:38:53 +0900

    test9 red

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -23,6 +23,12 @@ public class App {
                 System.out.println("명언: ");
                 System.out.println("작가: ");
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastId));
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                System.out.println("2 / 작자미상 / 과거에 집착하지 마라.");
+                System.out.println("1 / 작자미상 / 현재를 사랑하라.");
+
             }
         }
 
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -118,5 +118,30 @@ public class FirstTest {
                 .contains("2번 명언이 등록되었습니다.");
     }
 
+    @Test
+    @DisplayName("목록 - 명언 2개 입력하면 입력된 명언들이 출력된다.")
+    void t9() {
+        //입력
+        String out=TestBot.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                """);
+
+        //출력
+        assertThat(out)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.")
+                .contains("번호 / 작가 / 명언")
+                .contains("----------------------")
+                .containsSubsequence("2 / 작자미상 / 과거에 집착하지 마라."
+                        ,"1 / 작자미상 / 현재를 사랑하라.");
+    }
+
+
+
 
 }
