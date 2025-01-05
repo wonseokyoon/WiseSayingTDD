c2fe455128da299c6bebfbe8cdff7486f15ff58c
wonseokyoon <01088115030a@gmail.com>
2025-01-05 14:08:43 +0900

    test4 blue

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App {
         System.out.println("==명언 앱==");
         System.out.println("명령) ");
         System.out.println("명령) ");
+        System.out.println("명령) ");
         System.out.println("명언: ");
         System.out.println("작가: ");
         System.out.println("명언앱을 종료합니다.");
