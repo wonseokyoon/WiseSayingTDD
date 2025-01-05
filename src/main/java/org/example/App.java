4f8d8bff727aedb70ed3b871ddcf1cabee235d06
wonseokyoon <01088115030a@gmail.com>
2025-01-05 14:28:06 +0900

    test8 green

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -23,6 +23,7 @@ public class App {
                 System.out.println("명언: ");
                 System.out.println("작가: ");
                 System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println("2번 명언이 등록되었습니다.");
             }
         }
 
