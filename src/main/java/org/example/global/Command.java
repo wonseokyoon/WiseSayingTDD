fa08bf4f8236c42fa7197dbdccba1f5c9d53d200
wonseokyoon <01088115030a@gmail.com>
2025-01-06 10:25:03 +0900

    command test2 red

--- src/main/java/org/example/global/Command.java
+++ src/main/java/org/example/global/Command.java
@@ -2,6 +2,12 @@ package org.example.global;
 
 public class Command {
     // 쪼개기 작업
+    public Command(String cmd){
 
 
+    }
+
+    public String getActionName() {
+        return "삭제";
+    }
 }
--- src/test/java/global/CommandTest.java
+++ src/test/java/global/CommandTest.java
@@ -4,11 +4,22 @@ import org.example.global.Command;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 public class CommandTest {
 
     @Test
     @DisplayName("command Test 최초 테스트")
     void t1(){
-        Command cmd=new Command();
+        Command cmd=new Command("");
+    }
+
+    @Test
+    @DisplayName("action 네임을 얻어올 수 있다. - 삭제?id=1이면 삭제가 나와야한다")
+    void t2(){
+        Command cmd=new Command("삭제?id=1");
+        String actionName=cmd.getActionName();
+
+        assertThat(actionName).isEqualTo("삭제");
+
     }
 }
