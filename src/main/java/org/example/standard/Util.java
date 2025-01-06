bff84762eae4987cc4b6deb7971a1c4007532c51
wonseokyoon <01088115030a@gmail.com>
2025-01-06 12:49:29 +0900

    FileTest 1

---
+++ src/main/java/org/example/standard/Util.java
@@ -0,0 +1,13 @@
+package org.example.standard;
+
+public class Util {
+
+    public static class File{
+        public static void test(){
+            System.out.println("파일 유틸 테스트");
+        }
+
+
+    }
+
+}
---
+++ src/test/java/standard/FileTest.java
@@ -0,0 +1,15 @@
+package standard;
+
+import org.example.standard.Util;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class FileTest {
+
+    @Test
+    @DisplayName("최초의 파일 테스트")
+    void t1(){
+        Util.File.test();
+    }
+}
